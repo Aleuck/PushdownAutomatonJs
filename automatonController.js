b56106b43db3bbd0308568e28089248fe0989a9f
Alexandre Leuck <aleuck2004@gmail.com>
2013-06-28 00:52:57 -0300

    Initial commit

---
+++ automatonController.js
@@ -0,0 +1,414 @@
+/* This file is part of PushdownAutomatonJs
+ *
+ * PushdownAutomatonJs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PushdownAutomatonJs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PushdownAutomatonJs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+// Interface singleton (Controller)
+AUTOMATON_CONTROLLER = (function (M) {
+	var maxStackCount = 10,  // max number of stacks allowed
+		edit = true,         // is user is editing the states // false when executing
+	    stacksManagement,    // screen to choose the number of stacks
+	    alphabetsManagement, // screen to set the alphabets symbols
+	    statesManagement,    // screen to set the states
+	    editButtons,         // div containing the buttons to show when editing the states
+	    executeButtons,      // div containing the buttons to show when executing the program
+	    lAlph,               // label for the symbols of the alphabet definition
+	    lAuxAlph,            // label for the symbols of the auxiliar alphabet definition
+	    lStates,             // label for the list of states
+	    ulAlph,              // alphabet list on alphabet management
+	    ulAuxAlph,           // auxiliar alphabet list on alphabet management
+	    ulStates,            // list of states on states&rules management
+	    ulStacks,            // list containing the input queue and all the stacks
+	    iAlphSym,            // input field for the alphabet symbols
+	    iAuxAlphSym,         // input field for the auxiliar alphabet symbols
+	    bAddAlphSym,         // button to add symbol on input to the alphabet
+	    bAddAuxAlphSym,      // button to add symbol on input to the auxiliar alphabet
+	    bAlphabetsSubmit,    // button to finish editing the alphabet
+	    bExecution,          // button to switch to the execution interface
+	    bStep,               // button to execute a single step of the computation.
+	    bRunAll,             // button to execute the program till it reachs the final state (warning: possible infinte loop)
+	    bEditStates;         // button to leave execution mode and resume editing the program
+
+	// updateAlph (function to update the alphabet on interface during editing)
+	function updateAlph() {
+		var alph = M.getAlphabet(), symStrList = [];
+		alph.forEach(function (s) {
+			symStrList.push('<li class="symbol" title="click to remove">' + s + '</li>');
+		});
+		ulAlph.innerHTML = symStrList.join("\n");
+		symStrList = [];
+		alph.forEach(function (s) {
+			symStrList.push('<span class="symbol">' + s + '</span>');
+		});
+		lAlph.innerHTML = symStrList.join(", ");
+	}
+	// updateAuxAlph (function to update the auxiliar alphabet on interface during editing)
+	function updateAuxAlph() {
+		var alph = M.getAuxAlphabet(), symStrList = [];
+		alph.forEach(function (s) {
+			symStrList.push('<li class="symbol">' + s + '</li>');
+		});
+		ulAuxAlph.innerHTML = symStrList.join("\n");
+		symStrList = [];
+		alph.forEach(function (s) {
+			symStrList.push('<span class="symbol">' + s + '</span>')
+		});
+		lAuxAlph.innerHTML = symStrList.join(", ");
+	}
+	// updateStates (function to update the states on the interface during editing or execution)
+	// params: [edit = true] : set as false to disable edit
+	function updateStates() {
+		var i, j, k, s, stack, states = M.getStates(),
+		    alphabet = M.getAlphabet(),
+		    sAlphabet = alphabet.concat(M.getAuxAlphabet()),
+		    nStacks = M.getStackNumber(),
+		    nStates = states.length,
+		    queue = M.getQueue(),
+		    c_state = M.getCurrentState();
+		    stateListHtml = [],
+		    statesDefHtml = [],
+		    stacksListHtml = [];
+		for (i = 0; i < nStates; i += 1) {
+			statesDefHtml.push("q" + i.toString(10).sub());
+			// state list
+			stateListHtml.push('<li class="state');
+			if (c_state === i && !edit) {
+				stateListHtml.push(' current');
+			}
+			stateListHtml.push('" id="q' + i.toString(10) + '">');
+			stateListHtml.push('<h2>q' + i.toString(10).sub() + '</h2>');
+			if (edit && i > 0) {
+				stateListHtml.push('<button type="button" id="removestate_' + i + '" class="delete">x</button>');
+			}
+			stateListHtml.push('<ul class="rules"><h3>Rules</h3>');
+			for (j = 0; j < states[i].rules.length; j += 1) {
+				stateListHtml.push('<li>Π(q' + i.toString(10));
+				s = states[i].rules[j].X;
+				if (s === EMPTY) { s = '?'; }
+				if (s === ε) { s = 'ε'; }
+				stateListHtml.push(', ' + (s));
+				for (k = 0; k < nStacks; k += 1) {
+					s = states[i].rules[j].Y[k];
+					if (s === EMPTY) { s = '?'; }
+					if (s === ε) { s = 'ε'; }
+					stateListHtml.push(', ' + s);
+				}
+				stateListHtml.push(') = (q' + (states[i].rules[j].q < 0 ? 'f'.sub() : states[i].rules[j].q.toString().sub()) );
+				for (k = 0; k < nStacks; k += 1) {
+					s = states[i].rules[j].W[k];
+					if (s === ε) { s = 'ε'; }
+					stateListHtml.push(', ' + s);
+				}
+				stateListHtml.push(')');
+				if (edit) {
+					stateListHtml.push(' <button id="removerule_' + i + '_' + j + '" class="delete">x</button></li>');
+				}
+			}
+			stateListHtml.push('</ul>');
+			// New rule form
+			if (edit) {
+				stateListHtml.push('<div class="new_rule"><b>New rule:</b><br>');
+				stateListHtml.push('Π(q' + i.toString(10).sub());
+				stateListHtml.push(', <select id="q' + i.toString(10) + '_XRead">');
+				stateListHtml.push('<option value="' + ε.toString(10) + '">ε</option>');
+				stateListHtml.push('<option value="' + EMPTY.toString(10) + '">?</option>');
+				for (j = 0; j < alphabet.length; j += 1) {
+					stateListHtml.push('<option value="s' +  alphabet[j] + '">' + alphabet[j] + '</option>');
+					// The 's' character in the value property is intended to tell the program
+					// the programthe value is a symbol not a integer constant.
+				}
+				stateListHtml.push('</select>');
+				for (j = 0; j < nStacks; j += 1) {
+					stateListHtml.push(', <select id="q' + i.toString(10) + '_YRead_' + j.toString(10) + '">');
+					stateListHtml.push('<option value="' + ε.toString(10) + '">ε</option>');
+					stateListHtml.push('<option value="' + EMPTY.toString(10) + '">?</option>');
+					for (k = 0; k < sAlphabet.length; k += 1) {
+						stateListHtml.push('<option value="s' + sAlphabet[k] + '">' + sAlphabet[k] + '</option>');
+						// The 's' character in the value property is intended to tell the program
+						// the programthe value is a symbol not a integer constant.
+					}
+					stateListHtml.push('</select>');
+				}
+				stateListHtml.push(') = (<select id="q' + i.toString(10) + '_stateTo">');
+				for (j = 0; j < nStates; j += 1) {
+					stateListHtml.push('<option value="' + j.toString(10) + '">q' + j.toString(10).sub() + '</option>');
+				}
+				stateListHtml.push('<option value="' + FINAL + '">q<sub>f</sub></option></select>');
+				for (j = 0; j < nStacks; j += 1) {
+					stateListHtml.push(', <select id="q' + i.toString(10) + '_YWrite_' + j.toString(10) + '">');
+					stateListHtml.push('<option value="' + ε.toString(10) + '">ε</option>');
+					for (k = 0; k < sAlphabet.length; k += 1) {
+						stateListHtml.push('<option value="s' + sAlphabet[k] + '">' + sAlphabet[k] + '</option>');
+					}
+					stateListHtml.push('</select>');
+				}
+				stateListHtml.push(')<br><button type="button" id="addrule_' + i.toString(10) + '">save rule</button></div>');
+			}
+			stateListHtml.push('</li>');
+		}
+		statesDefHtml.push("q<sub>f</sub>");
+		stateListHtml.push('<li class="state');
+		if (c_state === -1 && !edit) {
+			stateListHtml.push(' current');
+		}
+		stateListHtml.push('" id="qf"><h2>q<sub>f</sub></h2></li>');
+		stateListHtml.push('<li style="clear: both; float:none; display: block;"></li>');
+		lStates.innerHTML  = statesDefHtml.join(", ");
+		ulStates.innerHTML = stateListHtml.join("");
+	}
+	// updateStacks (function to update the stack&queue interface during editing and execution)
+	// params: [edit = false] : set as true to enable editing the queue content
+	function updateStacks() {
+		var i, j, stackListHtml = [], queue = M.getQueue(), stack, nStacks = M.getStackNumber();
+		stackListHtml.push('<li><h2>X</h2><ul class="queue">');
+		for (i = 0; i < queue.length; i += 1) {
+			stackListHtml.push('<li class="symbol">' + queue[i] + '</li>');
+		}
+		stackListHtml.push("</ul></li>");
+		for (i = 0; i < nStacks; i += 1) {
+			stack = M.getStack(i);
+			stackListHtml.push('<li><h2>Y' + i.toString(10).sub() + '</h2><ul class="stack">');
+			for (j = 0; j < stack.length; j += 1) {
+				stackListHtml.push('<li class="symbol">' + stack[i].toString(10) + '</li>');
+			}
+			stackListHtml.push('</ul></li>');
+		}
+		if (!edit) {
+			stackListHtml.push('<br><input id="input_queue" type="text" placeholder="Set input queue"> <button type="button" id="set_queue">set</button>');
+		}
+		ulStacks.innerHTML = stackListHtml.join("");
+	}
+	function bAddAlphSym_onclick() {
+		if (iAlphSym && iAlphSym.value !== "") {
+			try {
+				M.addToAlphabet(iAlphSym.value);
+			}
+			catch (e) {
+				alert(e.message)
+			}
+			iAlphSym.value = "";
+			updateAlph();
+			iAlphSym.focus();
+		}
+	}
+	function bAddAuxAlphSym_onclick() {
+		var alph, symStrList = [];
+		if (iAuxAlphSym && iAuxAlphSym.value !== "") {
+			try {
+				M.addToAuxAlphabet(iAuxAlphSym.value);
+			}
+			catch (e) {
+				alert(e.message)
+			}
+			iAuxAlphSym.value = "";
+			updateAuxAlph();
+			iAuxAlphSym.focus();
+		}
+	}
+	function ulAlph_onclick(e) {
+		var elem, symbol;
+		e = e || event;
+		elem = e.target;
+		// if the element is of class "symbol"
+		if (-1 !== Array.prototype.indexOf.call(elem.classList, "symbol")) {
+			symbol = elem.textContent;
+			M.removeFromAlphabet(symbol);
+			updateAlph();
+		}
+	}
+	function ulAuxAlph_onclick(e) {
+		var elem, symbol;
+		e = e || event;
+		elem = e.target;
+		// if the element is of class "symbol"
+		if (-1 !== Array.prototype.indexOf.call(elem.classList, "symbol")) {
+			symbol = elem.textContent;
+			M.removeFromAuxAlphabet(symbol);
+			updateAuxAlph();
+		}
+	}
+	function bStackNumber_onclick() {
+		var stacksNumber = parseInt(iStacksNumber.value);
+		if (isNaN(stacksNumber) || stacksNumber < 0 || stacksNumber > maxStackCount) {
+			alert("The number of stacks must be a number from 0 to " + maxStackCount);
+		} else {
+			M.init(stacksNumber);
+			switchScreen("alphabets");
+		}
+	}
+	function bAlphabetsSubmit_onclick() {
+		updateStates();
+		updateStacks();
+		switchScreen("states");
+	}
+	function bNewState_onclick() {
+		M.newState();
+		updateStates();
+	}
+	function addRule(src) {
+		var i, x, y = [], w = [], dest, nStacks = M.getStackNumber()
+		dest = parseInt(document.getElementById('q' + src.toString(10) + '_stateTo').value, 10);
+		x = document.getElementById("q" + src.toString(10) + "_XRead").value;
+		x = x[0] === 's' ? x.slice(1) : parseInt(x, 10);
+		for (i = 0; i < nStacks; i += 1) {
+			y[i] = document.getElementById('q' + src.toString(10) + '_YRead_' + i.toString(10)).value;
+			y[i] = y[i][0] === 's' ? y[i].slice(1) : parseInt(y[i], 10);
+			w[i] = document.getElementById('q' + src.toString(10) + '_YWrite_' + i.toString(10)).value;
+			w[i] = w[i][0] === 's' ? w[i].slice(1) : parseInt(w[i], 10);
+		}
+		M.addStateRule(src, x, y, w, dest)
+		updateStates();
+	}
+	function removeRule(state, rule) {
+		M.removeStateRule(state, rule);
+		updateStates();
+	}
+	function removeState (state) {
+		try {
+			M.removeState(state);
+		}
+		catch (e) {
+			alert(e.message);
+		}
+		updateStates();
+	}
+	function ulStates_onclick(e) {
+		var i, elem, aId;
+		e = e || event;
+		elem = e.target;
+		aId = elem.id.split("_");
+		if (aId.length) { 
+			switch(aId[0]) {
+				case "addrule":
+					addRule(parseInt(aId[1], 10));
+					break;
+				case "removerule":
+					removeRule(parseInt(aId[1], 10), parseInt(aId[2], 10))
+					break;
+				case "removestate":
+					removeState(parseInt(aId[1], 10));
+					break;
+			}
+		}
+	}
+	function ulStacks_onclick (e) {
+		var elem, q;
+		e = e || event;
+		elem = e.target;
+		if (elem.id === "set_queue") {
+			q = document.getElementById("input_queue").value.split("");
+			M.setQueue(q);
+			updateStacks();
+		}
+	}
+	function bExecution_onclick () {
+		edit = false;
+		updateStates();
+		updateStacks();
+		switchScreen('execution');
+	}
+	function bStep_onclick() {
+		var s, c = M.getCurrentState();
+		if (c >= 0) {
+			s = M.step();
+		} else {
+			M.resetExec();
+		}
+		updateStates();
+		updateStacks();
+		switch (s) {
+			case FINAL:
+				alert('Computation complete / Word accepted');
+				break;
+			case REJECT:
+				alert('Computation failed / Word rejected');
+				break;
+		}
+	}
+	function bEditStates_onclick () {
+		edit = true;
+		updateStates();
+		updateStacks();
+		switchScreen("states")
+	}
+	function switchScreen(screen) {
+		stacksManagement.style.display    = "none";
+		alphabetsManagement.style.display = "none";
+		statesManagement.style.display    = "none";
+		editButtons.style.display         = "none";
+		executeButtons.style.display      = "none";
+		switch (screen) {
+			case "stacks":
+				stacksManagement.style.display    = "block";
+				break;
+			case "alphabets":
+				alphabetsManagement.style.display = "block";
+				break;
+			case "states":
+				//ulStates.onclick = ulStates_onclick;
+				editButtons.style.display         = "block";
+				statesManagement.style.display    = "block";
+				break;
+			case "execution":
+				//ulStates.onclick = null;
+				executeButtons.style.display      = "block";
+				statesManagement.style.display    = "block";
+				break;
+		}
+	}
+	return {
+		init: function () {
+			// GET SCREEN INTERFACE ELEMENTS
+			stacksManagement    = document.getElementById("stacks_management");
+			alphabetsManagement = document.getElementById("alphabets_management");
+			statesManagement    = document.getElementById("states_management");
+			// GET I/O INTERFACE ELEMENTS
+			lAlph            = document.getElementById("alphabet");
+			lAuxAlph         = document.getElementById("aux_alphabet");
+			lStates          = document.getElementById("states");
+			ulAlph           = document.getElementById("alphabet_list");
+			ulAuxAlph        = document.getElementById("aux_alphabet_list");
+			iAlphSym         = document.getElementById("input_alphabet_symbol");
+			iAuxAlphSym      = document.getElementById("input_aux_alphabet_symbol");
+			iStacksNumber    = document.getElementById("stacks_number");
+			bAddAlphSym      = document.getElementById("add_alphabet");
+			bAddAuxAlphSym   = document.getElementById("add_aux_alphabet");
+			bStacksNumber    = document.getElementById("set_stacks_number");
+			bAlphabetsSubmit = document.getElementById("alphabets_submit");
+			bNewState        = document.getElementById("new_state");
+			ulStates         = document.getElementById("state_list");
+			ulStacks         = document.getElementById("queuestacks_list");
+			bExecution       = document.getElementById("goto_execution");
+			bStep            = document.getElementById("run_step");
+			bRunAll          = document.getElementById("run_all");
+			editButtons      = document.getElementById("edit_buttons");
+			executeButtons   = document.getElementById("execute_buttons");
+			bEditStates      = document.getElementById("edit_states");
+			// SET INTERFACE HANDLERS
+			switchScreen("stacks");
+			bAddAlphSym.onclick      = bAddAlphSym_onclick;
+			bAddAuxAlphSym.onclick   = bAddAuxAlphSym_onclick;
+			ulAlph.onclick           = ulAlph_onclick;
+			ulAuxAlph.onclick        = ulAuxAlph_onclick;
+			bStacksNumber.onclick    = bStackNumber_onclick;
+			bAlphabetsSubmit.onclick = bAlphabetsSubmit_onclick;
+			bNewState.onclick        = bNewState_onclick;
+			ulStates.onclick         = ulStates_onclick;
+			ulStacks.onclick         = ulStacks_onclick;
+			bExecution.onclick       = bExecution_onclick;
+			bStep.onclick            = bStep_onclick;
+			bEditStates.onclick      = bEditStates_onclick;
+		}
+	};
+}(AUTOMATON_MODEL));
---
+++ automatonModel.js
@@ -0,0 +1,335 @@
+/* This file is part of PushdownAutomatonJs
+ *
+ * PushdownAutomatonJs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PushdownAutomatonJs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PushdownAutomatonJs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// CONSTANTS
+var ε = -1,      // read/write nothing
+    EMPTY = -2,  // read empty
+    FINAL = -1,  // final state id
+    REJECT = -2; // reject "state" id (when it stops not in the final state).
+// Automaton singleton (Model)
+var AUTOMATON_MODEL = (function () {
+	// Private variables.
+	var Σ, V, X, Y, nStacks, Q = 0, q, prev_q;
+	// Σ      := alphabet
+	// V      := auxiliar alphabet
+	// X      := queue
+	// Y      := array of stacks
+	// Q      := states
+	// F      := array of final states indexes
+	// q      := current state
+	// prev_q := last state beforing stopping
+	function save() {
+		var i, j, save = {};
+		save.stacks = nStacks;
+		save.alphabet = Σ.slice(0);
+		save.aux_alphabet = V.slice(0);
+		save.states = [];
+		for (i = 0; i < Q.length; i += 1) {
+			save.states[i] = {};
+			for (j in Q[i]) {
+				if (Q[i].hasOwnProperty(j)) {
+					save.states[i][j] = Q[i][j];
+				}
+			}
+		};
+		return JSON.stringify(save);
+	}
+	function restore(json) {
+		var i, save = JSON.parse(json);
+		q = 0;
+		Σ = save.alphabet.slice(0);
+		V = save.aux_alphabet.slice(0);
+		for (i = 0; i < save.states.length; i += 1) {
+			Q[i] = new State();
+			Q[i].id = i;
+			save.states[i].rules.forEach(function (rule) {
+				Q[i].addRule(rule.q, rule.X, rule.Y, rule.W);
+			});
+		}
+	}
+	function runAll() {
+		if (q < 0) q = 0;
+		while (q >= 0 && q < Q.length) {
+			prev_q = q;
+			q = Q[q].execute();
+		}
+		return (q === -1);
+	}
+	function step() {
+		if (q >= 0 && q < Q.length) {
+			prev_q = q;
+			q = Q[q].execute();
+		} else {
+			q = 0;
+		}
+		return q;
+	}
+	function resetExec() {
+			var i;
+			q = 0; 
+			X = [];
+			Y = [];
+			for (i = 0; i < nStacks; i += 1) {
+				Y[i] = [];
+			}
+		}
+	// State Constructor
+	function State() {
+		this.rules = [];
+	}
+	State.prototype = {
+		id: 0,
+		rules: null,
+		execute: function () {
+			var r, rulesToFollow;
+			// filter all the rules for rules that match the current state.
+			rulesToFollow = this.rules.filter(function (rule) {
+				var i, sX, sY = [], follow = true; // assume the rule can be applied
+				// Getting the first symbol in the queue and in each stack
+				if (X.length === 0) {
+					sX = EMPTY;
+				} else {
+					sX = X[0];
+				}
+				sY = [];
+				for (i = 0; i < Y.length; i += 1) {
+					if (Y[i].length === 0) {
+						sY[i] = EMPTY;
+					} else {
+						sY[i] = Y[i][(Y[i].length - 1)]; // get last
+					}
+				}
+				// Comparing the symbols to check if there is a rule that does not match
+				if (rule.X !== ε && rule.X !== sX) {
+					// queue symbol does not mach -> can't apply rule
+					follow = false;
+				}
+				for (i = 0; i < rule.Y.length; i += 1) {
+					if (rule.Y[i] !== ε && rule.Y[i] !== sY[i]) {
+						// stack symbol does not match -> can't apply rule
+						follow = false;
+					}
+				}
+				return follow;
+			});
+			if (rulesToFollow.length === 0) {
+				// no rule matches, reject the word
+				return REJECT;
+			}
+			if (rulesToFollow.length === 1) {
+				// 1 rule match, execute it:
+				r = rulesToFollow[0];
+				if (r.X !== ε && r.X !== EMPTY) {
+					// reading (removing) symbol read from the queue
+					X = X.slice(1);
+				}
+				for (i = 0; i < Y.length; i += 1) {
+					if (r.Y[i] !== ε && r.Y[i] !== EMPTY) {
+						// reading (removing) the symbol read from the stack
+						Y[i].pop();
+					}
+					if (r.W[i] !== ε) {
+						// writing (pushing) symbol to the stack
+						Y[i].push(r.W[i]);
+					}
+				}
+				// return destination state
+				return r.q;
+			}
+			// more than 1 rule matches, throw error
+			throw new Error("Ambiguous rules on q" + this.id.toString(10));
+			return REJECT;
+		},
+		addRule: function (q, x, y, w) {
+			var i, e_rule, n_rule, ruleIdx = this.rules.length, ruleIsAmbiguous = true;
+			if (q > Q.length) throw new Error("Invalid rule: destination state does not exist.");
+			this.rules[ruleIdx] = { 'q': q, 'X': x || ε, 'Y': [], 'W': [] };
+			n_rule = this.rules[ruleIdx];
+			for (i = 0; i < Y.length; i += 1) {
+				n_rule.Y[i] = (y && y[i]) || ε;
+				n_rule.W[i] = (w && w[i]) || ε;
+			}
+			for (i = 0; i < ruleIdx; i += 1) {
+				e_rule = this.rules[i];
+				if (e_rule.X !== ε && n_rule.X !== ε && e_rule.X !== n_rule.X) continue;
+				if (function () {
+					var i, ambiguity = true;
+					for (i = 0; i < e_rule.Y.length; i += 1) {
+						if (e_rule.Y[i] !== ε && n_rule.Y[i] !== ε && n_rule.Y[i] !== e_rule.Y[i]) {
+							ambiguity = false;
+							continue;
+						}
+					}
+					return ambiguity;
+				}()) {
+					this.rules.pop();
+					throw new Error("This rule causes ambiguity.");
+					return;
+				}
+			}
+		}
+	};
+	function removeState (index) {
+		var i, j;
+		for (i = 0; i < Q.length; i += 1) {
+			if (i !== index) {
+				for (j = 0; j < Q[i].rules.length; j += 1) {
+					if (Q[i].rules[j].q === index) {
+						throw new Error("You can't delete a state while there are other states with rules pointing to it.");
+						return;
+					}
+				}
+			}
+		}
+		for (i = 0; i < Q.length; i += 1) {
+			if (i !== index) {
+				for (j = 0; j < Q[i].rules.length; j += 1) {
+					if (Q[i].rules[j].q > index) {
+						Q[i].rules[j].q -= 1;
+					}
+				}
+			}
+		}
+		Q = Q.slice(0,index).concat(Q.slice(index + 1));
+		for (i = 0; i < Q.length; i += 1) {
+			Q[i].id = i;
+		}
+	}
+	// Alphabet Management
+	function addToAlphabet(symbol) {
+		if ("string" !== typeof symbol) {
+			throw new Error("Expecting string instead of " + (typeof symbol));
+			return;
+		}
+		if (-1 !== V.indexOf(symbol)) {
+			throw new Error("Can't add symbol to Σ: V already contains the given symbol.");
+			return;
+		}
+		if ("" !== symbol && -1 === Σ.indexOf(symbol)) {
+			Σ.push(symbol);
+		}
+	}
+	function addToAuxAlphabet(symbol) {
+		if ("string" !== typeof symbol) {
+			throw new Error("Expecting string instead of " + (typeof symbol));
+			return;
+		}
+		if (-1 !== Σ.indexOf(symbol)) {
+			throw new Error("Can't add symbol to V: Σ already contains the given symbol.");
+			return;
+		}
+		if (-1 === V.indexOf(symbol)) {
+			V.push(symbol);
+		}
+	}
+	function removeFromAlphabet(symbol) {
+		var aux;
+		if ("string" !== typeof symbol) {
+			throw new Error("Expecting string instead of " + (typeof symbol));
+			return;
+		}
+		var index = Σ.indexOf(symbol);
+		if (-1 !== index) {
+			aux = Σ.slice(0, index).concat(Σ.slice(index + 1));
+			Σ = aux;
+		}
+	}
+	function removeFromAuxAlphabet(symbol) {
+		var aux;
+		if ("string" !== typeof symbol) {
+			throw new Error("Expecting string instead of " + (typeof symbol));
+			return;
+		}
+		var index = V.indexOf(symbol);
+		if (-1 !== index) {
+			aux = V.slice(0, index).concat(V.slice(index + 1));
+			V = aux;
+		}
+	}
+	// Queue
+	function setQueue(q) {
+		X = [];
+		// check each symbol if it is on the alphabet before adding it to the X
+		q.forEach(function (symbol) {
+			if (-1 !== Σ.indexOf(symbol)) {
+				X.push(symbol);
+			}
+		});
+	}
+	// Public methods
+	return {
+		init: function (numberOfStacks) {
+			var i;
+			Σ = [];
+			V = [];
+			Q = []; // STATES
+			//q0 = new State();
+			Q[0] = new State();
+			Q[0].id = 0;
+			q = 0;
+			F = [];
+			X = []; // QUEUE
+			Y = []; // STACKS
+			if ("number" !== typeof numberOfStacks) {
+				numberOfStacks = 2;
+			}
+			nStacks = numberOfStacks;
+			for (i = 0; i < nStacks; i += 1) {
+				Y[i] = [];
+			}
+		},
+		setQueue: setQueue,
+		getQueue: function () { return X.slice(0); }, // .slice(0) will create a copy of the array
+		getAlphabet: function () { return Σ.slice(0); },
+		getAuxAlphabet: function () { return V.slice(0); },
+		getStates: function() { return Q.slice(0); },
+		getStack: function (index) { return Y[index].slice(0); },
+		addToAlphabet: addToAlphabet,
+		addToAuxAlphabet: addToAuxAlphabet,
+		removeFromAlphabet: removeFromAlphabet,
+		removeFromAuxAlphabet: removeFromAuxAlphabet,
+		newState: function () {
+			var id = Q.length;
+			var newState = new State();
+			newState.id = id;
+			Q[id] = newState;
+			return newState;
+		},
+		getStackNumber: function() { return Y.length; },
+		/* setStateRules
+		 * s: source state
+		 * x: read from x
+		 * y: (array) read from Y
+		 * w: (array) write to Y
+		 * d: destination state
+		 */
+		addStateRule: function (s, x, y, w, d) {
+			if (Q[s]) Q[s].addRule(d, x, y, w);
+		},
+		removeStateRule: function (state, index) {
+			if (Q[state] && Q[state].rules[index]) {
+				Q[state].rules = Q[state].rules.slice(0, index).concat(Q[state].rules.slice(index + 1));
+			}
+		},
+		getCurrentState: function () { return q; },
+		getPreviousState: function () { return prev_q; },
+		resetExec: resetExec,
+		step: step,
+		run: runAll,
+		removeState: removeState,
+		save: save,
+		restore: restore
+	};
+}());
